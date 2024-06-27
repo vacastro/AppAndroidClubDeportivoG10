21470575731f3016d489337b369ce44587f90765
vacastro <56000486+vacastro@users.noreply.github.com>
2024-06-27 14:16:17 -0300

    sqlite, metodos insertar usuario, etc, main activity, login, implementacion de metodo

--- ClubDeportivo_Grupo10/app/src/main/java/com/example/clubdeportivo_grupo10/MainActivity.kt
+++ ClubDeportivo_Grupo10/app/src/main/java/com/example/clubdeportivo_grupo10/MainActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -14,21 +16,54 @@ import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var clubDeportivo: sqlHelper
+    private lateinit var sqlHelper: sqlHelper
+    private lateinit var emailEditText:EditText
+    private lateinit var claveEditText: EditText
+    private lateinit var btnIngresar: Button
+
+
      override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+         // Si elige la opcion Registrarse, va a la publicidad
         val btnRegister = findViewById<Button>(R.id.btnRegistrarse)
         btnRegister.setOnClickListener {
-            // Aquí se define la acción al hacer clic en el botón registrarse
+
             val intent = Intent(this, PublicidadActivity::class.java)
             startActivity(intent)
         }
 
-         val btnIngresar = findViewById<Button>(R.id.btnIngresar)
-         btnIngresar.setOnClickListener {
-            val intent = Intent(this, PrincipalActivity::class.java)
-         startActivity(intent)}
+         // Si elige la opcion Ingresar, debe verificar los datos en la base
+         //TODO falta hacer validaciones de datos en login, si esta vacio, etc y mostrar el error
 
+         clubDeportivo= sqlHelper(this)
+         emailEditText= findViewById(R.id.textInputEmail)
+         claveEditText=findViewById(R.id.textInputPass)
+         btnIngresar = findViewById<Button>(R.id.btnIngresar)
+
+         btnIngresar.setOnClickListener {
+
+             val email= emailEditText.text.toString()
+             val clave = claveEditText.text.toString()
+
+             val chequearEmail = clubDeportivo.existeEmail(email)
+
+             if(chequearEmail != true){
+                 Toast.makeText(this, "User not found", Toast.LENGTH_SHORT).show();
+             }else{
+                 val claveDB = clubDeportivo.obtenerClavePorEmail(email)
+
+                 if(clave.equals(claveDB)){
+
+                     val intent = Intent(this, PrincipalActivity::class.java)
+                     startActivity(intent)
+                 } else{
+                     Toast.makeText(this, "Invalid login", Toast.LENGTH_SHORT).show();
+                 }
+
+             }
+         }
     }
 }
---
+++ ClubDeportivo_Grupo10/app/src/main/java/com/example/clubdeportivo_grupo10/sqlHelper.kt
@@ -0,0 +1,112 @@
+package com.example.clubdeportivo_grupo10
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class sqlHelper (context:Context): SQLiteOpenHelper (context, "clubDeportivo.db",null,1 ){
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        val CREATE_TABLE_USUARIOS = """
+            CREATE TABLE Usuarios (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nombre TEXT NOT NULL,
+                email TEXT NOT NULL,
+                telefono TEXT NOT NULL,
+                clave TEXT NOT NULL
+            );
+        """
+
+        val CREATE_TABLE_CONTRATOS = """
+            CREATE TABLE Contratos (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                usuario INTEGER NOT NULL,
+                actividad TEXT NOT NULL,
+                fecha_alta TEXT NOT NULL,
+                FOREIGN KEY (usuario) REFERENCES Usuarios(id)
+            );
+        """
+
+        val CREATE_TABLE_PAGOS = """
+            CREATE TABLE Pagos (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                usuario INTEGER NOT NULL,
+                fecha_vencimiento TEXT NOT NULL,
+                fecha_pago TEXT,
+                importe REAL NOT NULL,
+                FOREIGN KEY (usuario) REFERENCES Usuarios(id)
+            );
+        """
+
+        db?.execSQL(CREATE_TABLE_USUARIOS)
+        db?.execSQL(CREATE_TABLE_CONTRATOS)
+        db?.execSQL(CREATE_TABLE_PAGOS)
+
+        db?.execSQL("INSERT INTO Usuarios (nombre, email, telefono, clave) VALUES ('Agustin Guarmes', 'agustin@gmail.com', '1234567890', 'pass123')")
+        db?.execSQL("INSERT INTO Usuarios (nombre, email, telefono, clave) VALUES ('Martin Castanio', 'martin@gmail.com', '1234567890', 'pass123')")
+        db?.execSQL("INSERT INTO Usuarios (nombre, email, telefono, clave) VALUES ('Selene Rodriguez', 'selene@gmail.com', '1234567890', 'pass123')")
+        db?.execSQL("INSERT INTO Usuarios (nombre, email, telefono, clave) VALUES ('Vero Castro', 'vero@gmail.com', '1234567890', 'pass123')")
+
+        db?.execSQL("INSERT INTO Contratos (usuario, actividad, fecha_alta) VALUES (1, 'Socio', '2024-02-01')")
+        db?.execSQL("INSERT INTO Contratos (usuario, actividad, fecha_alta) VALUES (2, 'Futbol', '2024-03-01')")
+        db?.execSQL("INSERT INTO Contratos (usuario, actividad, fecha_alta) VALUES (3, 'Socio', '2024-05-01')")
+        db?.execSQL("INSERT INTO Contratos (usuario, actividad, fecha_alta) VALUES (4, 'Yoga', '2024-05-01')")
+
+        db?.execSQL("INSERT INTO Pagos (usuario, fecha_vencimiento, fecha_pago, importe) VALUES (1, '2024-02-01', '2024-02-05', 19999.0)")
+        db?.execSQL("INSERT INTO Pagos (usuario, fecha_vencimiento, fecha_pago, importe) VALUES (1, '2024-03-01', '2024-03-05', 19999.0)")
+        db?.execSQL("INSERT INTO Pagos (usuario, fecha_vencimiento, fecha_pago, importe) VALUES (1, '2024-04-01', '2024-04-05', 19999.0)")
+        db?.execSQL("INSERT INTO Pagos (usuario, fecha_vencimiento, fecha_pago, importe) VALUES (1, '2024-05-01', '2024-05-05', 19999.0)")
+        db?.execSQL("INSERT INTO Pagos (usuario, fecha_vencimiento, fecha_pago, importe) VALUES (1, '2024-06-01', '2024-06-05', 19999.0)")
+
+
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
+        db?.execSQL("DROP TABLE IF EXISTS Usuarios")
+        db?.execSQL("DROP TABLE IF EXISTS Contratos")
+        db?.execSQL("DROP TABLE IF EXISTS Pagos")
+        onCreate(db)
+    }
+
+
+    //Metodo para hacer un registro de usuarios
+    fun insertarUsuario(nombre:String, email:String, telefono:String, clave:String){
+        val datos =ContentValues().apply {
+            put("nombre", nombre);
+            put("email", email);
+            put("telefono", telefono);
+            put("clave", clave);
+        };
+
+        val db= this.writableDatabase;
+        db.insert("Usuarios",null, datos);
+        db.close();
+    }
+
+    // Metodo para obtener la clave dado un email
+    fun obtenerClavePorEmail(email: String): String? {
+        val db = this.readableDatabase
+        val cursor = db.rawQuery("SELECT clave FROM Usuarios WHERE email = ?", arrayOf(email))
+        var clave: String? = null
+
+        if (cursor.moveToFirst()) {
+            clave = cursor.getString(0)
+        }
+        cursor.close()
+        db.close()
+        return clave
+    }
+
+    // Metodo para verificar si un email existe
+    fun existeEmail(email: String): Boolean {
+        val db = this.readableDatabase
+        val cursor = db.rawQuery("SELECT 1 FROM Usuarios WHERE email = ?", arrayOf(email))
+        val exists = cursor.moveToFirst()
+        cursor.close()
+        db.close()
+        return exists
+    }
+
+
+}
