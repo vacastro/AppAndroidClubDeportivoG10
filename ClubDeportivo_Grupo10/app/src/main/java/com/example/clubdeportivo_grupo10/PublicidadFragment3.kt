dd4f3f4a03e5bb6e620465b4f0c3c05d6a6b0d1e
Martin Castaño <castanio.m@gmail.com>
2024-06-28 16:28:11 -0300

    funcion Registrar Usuario agregado

--- ClubDeportivo_Grupo10/app/src/main/java/com/example/clubdeportivo_grupo10/PublicidadFragment3.kt
+++ ClubDeportivo_Grupo10/app/src/main/java/com/example/clubdeportivo_grupo10/PublicidadFragment3.kt
@@ -1,18 +1,18 @@
 package com.example.clubdeportivo_grupo10
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 
 class PublicidadFragment3: Fragment() {
 
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
--- ClubDeportivo_Grupo10/app/src/main/java/com/example/clubdeportivo_grupo10/RegistroActivity.kt
+++ ClubDeportivo_Grupo10/app/src/main/java/com/example/clubdeportivo_grupo10/RegistroActivity.kt
@@ -4,10 +4,12 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class RegistroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,11 +27,22 @@ class RegistroActivity : AppCompatActivity() {
 
         val registrarseButton = findViewById<Button>(R.id.btnRegistrarse)
         registrarseButton.setOnClickListener {
-            val intent = Intent(this, ContratarActivity::class.java)
-            startActivity(intent)
-            finish() // Opcional: finalizar RegistroActivity si no quieres que esté en el back stack
-        }
+            val name = findViewById<TextInputEditText>(R.id.textInputName).text.toString()
+            val email = findViewById<TextInputEditText>(R.id.textInputEmail).text.toString()
+            val phone = findViewById<TextInputEditText>(R.id.textInputPhone).text.toString()
+            val pass = findViewById<TextInputEditText>(R.id.textInputPass).text.toString()
+            val repeatedPass = findViewById<TextInputEditText>(R.id.textInputRepetPass).text.toString()
 
+            if(passIsValid(pass, repeatedPass)) {
+                val db = sqlHelper(this)
+                db.insertarUsuario(name, email, phone, pass)
+                val intent = Intent(this, ContratarActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show()
+            }
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -37,5 +50,8 @@ class RegistroActivity : AppCompatActivity() {
             insets
         }
 
+    }
+    private fun passIsValid(pass: String, repeatedPass: String): Boolean {
+        return pass == repeatedPass
     }
 }
